edc60b871b8c2d2130f3117d8f89703915f162c0
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-09-17 10:02:40 -0700

    Login, logout, and check the session.

--- server/app.py
+++ server/app.py
@@ -19,22 +19,18 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
@@ -47,11 +43,52 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource): 
+    def post(self):
+        username = request.json.get('username')
+
+        if not username:
+            return {'message': 'Username is required'}, 400
+
+        user = User.query.filter_by(username=username).first()
+
+        response = make_response(user.to_dict(), 200)
+
+        if user:
+            session['user_id'] = user.id
+            return response
+        else:
+            return {'message': 'User not found'}, 404
+        
+class Logout(Resource):
+    def delete(self):
+        # Check if the user is logged in
+        if 'user_id' in session:
+            # Remove the user_id from the session
+            session.pop('user_id', None)
+            response = make_response('No Content', 204)
+            return response
+        else:
+            return {'message': 'User not logged in'}, 401  # 401 Unauthorized
+        
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            response = make_response({}, 401)
+            return response
+        
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
